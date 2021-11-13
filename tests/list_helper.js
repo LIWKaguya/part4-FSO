49215fcd6ffa71a203c8e0889c015d00f68bfdeb
LIWKaguya <hungle120901@gmail.com>
2021-11-14 00:33:33 +0200

    Exercise 4.6-4.7

--- tests/list_helper.js
+++ tests/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = () => {
     return 1
 }
@@ -13,7 +15,56 @@ const totalLikes = (blogs) => {
     return sumLikes 
 }
 
+const favoriteBlog = (blogs) => {
+    var selected = {}
+    const mostLikes = Math.max(...blogs.map(blog => blog.likes))
+    blogs.forEach(blog => {
+        if(blog.likes === mostLikes) {
+            selected.title = blog.title
+            selected.author = blog.author
+            selected.likes = blog.likes
+        }
+    }) 
+    return selected
+}
+
+const mostBlogs = (blogs) => {
+    const selected = {}
+    const freqAuthors = _.countBy(blogs.map(blog => blog.author))
+    const maxBlogs = Math.max(...Object.values(freqAuthors))
+    for(const [key, value] of Object.entries(freqAuthors)) {
+        if(value === maxBlogs) {
+            selected.author = key
+            selected.blogs = value
+        }
+    }
+    return selected
+}
+
+const mostLikes = (blogs) => {
+    const selected = {}, total = {}
+    blogs.forEach(blog => {
+        if(!total[blog.author]) {
+            total[blog.author] = blog.likes
+        }
+        else {
+            total[blog.author] += blog.likes
+        }
+    })
+    const maxLikes = Math.max(...Object.values(total))
+    for(const [key, value] of Object.entries(total)) {
+        if(value === maxLikes) {
+            selected.author = key
+            selected.likes = value
+        }
+    }
+    return selected
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
